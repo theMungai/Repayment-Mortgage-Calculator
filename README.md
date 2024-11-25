8307a907a780c3535caac7d0901402e21a92ef57
theMungai <mungaithesimon@gmail.com>
2024-11-25 18:08:47 +0300

    Mortgage Repayment Calculator

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # Kenyan-Mortgage-Calculator
+
+## Table of contents
+
+  - [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [My process](#my-process)
+  - [Built with](#built-with)
+  - [Author](#author)
+  - [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Input mortgage information and see monthly repayment and total repayment amounts after submitting the form
+- See form validation messages if any field is incomplete
+- Complete the form only using their keyboard
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![](/screenshots/Desktop.png)
+![](/screenshots/Mobile-device.png)
+
+
+### Links
+
+- Solution URL: [Mortgage repayment - Github ](https://github.com/theMungai)
+- Live Site URL: [Mortgage Repayment Calculator](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- JavaScript
+
+
+## Author
+
+- Website - [theMungai](https://github.com/theMungai)
---
+++ script.js
@@ -0,0 +1,113 @@
+const mortgageAmount = document.querySelector("#amount");
+const mortgageTerm = document.querySelector("#term");
+const interestRate = document.querySelector("#interest-rate");
+const emptyResults = document.querySelector(".empty-results")
+const resultsCalculation = document.querySelector(".results-calculation");
+const errorMessage = document.querySelectorAll(".error-message");
+
+
+const repaymentDiv = document.querySelector(".repayment");
+const interestOnlyDiv = document.querySelector(".interest-only");
+const repaymentRadio = document.querySelector(".repayment-radio");
+const interestRadio = document.querySelector(".interest-radio");
+
+
+const calculateButton = document.querySelector(".calculate-button");
+calculateButton.addEventListener("click", () => {
+    resultsCalculation.style.display = "block";
+    emptyResults.style.display = "none";
+
+    const amountSpan = document.querySelector(".amount-span");
+    const rateSpan = document.querySelector(".rate-span");
+    const termSpan = document.querySelector(".term-span");
+
+    if(mortgageAmount.value === ""){
+        showError("mortgage-amount-error","This field is required");
+        amountSpan.style.color = "white";
+        amountSpan.style.backgroundColor = "hsl(4, 69%, 50%)";
+        mortgageAmount.style.borderColor ="hsl(4, 69%, 50%)";
+
+    }
+
+    if (mortgageTerm.value === ""){
+        showError("mortgage-term-error","This field is required");
+        termSpan.style.color = "white";
+        termSpan.style.backgroundColor = "hsl(4, 69%, 50%)";
+        mortgageTerm.style.borderColor ="hsl(4, 69%, 50%)";
+
+    }
+    
+    if (interestRate.value === ""){
+        showError("interest-rate-error","This field is required");
+        rateSpan.style.color = "white";
+        rateSpan.style.backgroundColor = "hsl(4, 69%, 50%)";
+        interestRate.style.borderColor ="hsl(4, 69%, 50%)";
+    }
+
+    if(!repaymentRadio.checked && !interestRadio.checked){
+        showError("mortgage-type-error", "This field is required")
+    }
+
+});
+
+// Calculations if user clicks repayment div
+const totalPayments = document.querySelector(".js-total-payment")
+const monthlyPayment = document.querySelector(".js-monthly-payment");
+repaymentDiv.addEventListener("click", () => {
+
+    monthlyPayment.innerHTML = `KShs  ${parseFloat(
+    (mortgageAmount.value * (interestRate.value / 1200) * (1 + (interestRate.value / 1200)) ** (mortgageTerm.value * 12)) / 
+    ((1 + interestRate.value / 1200 ) ** (mortgageTerm.value * 12) - 1)).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
+
+    totalPayments.innerHTML = `KShs  ${parseFloat(
+        ((mortgageAmount.value * (interestRate.value / 1200) * (1 + (interestRate.value / 1200)) ** (mortgageTerm.value * 12)) / 
+        ((1 + interestRate.value / 1200 ) ** (mortgageTerm.value * 12) - 1)) * mortgageTerm.value * 12)
+        .toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
+});
+
+
+
+// Calculations if user clicks Interest Only div
+interestOnlyDiv.addEventListener("click", () => {
+    document.querySelector(".monthly-repayments").style.display = "none";
+    document.querySelector(".horizontal").style.display = "none";
+    document.querySelector(".total-repayments p").textContent = "Total interest over the term";
+    const totalInterest = document.querySelector(".total-repayments h3");
+    totalInterest.style.color = "hsl(61, 70%, 52%)"
+    totalInterest.style.fontSize = "1.5rem"
+
+    totalInterest.textContent = `KShs ${parseFloat((
+        ((mortgageAmount.value * (interestRate.value / 1200) * (1 + (interestRate.value / 1200)) ** (mortgageTerm.value * 12)) / 
+        ((1 + interestRate.value / 1200 ) ** (mortgageTerm.value * 12) - 1)) * mortgageTerm.value * 12) 
+        - mortgageAmount.value).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`
+});
+
+
+//  Checking Radio Boxes when divs are clicked
+
+function checkRadio(){
+    repaymentDiv.addEventListener("click", () => {
+        repaymentRadio.checked = true
+    })
+    
+    interestOnlyDiv.addEventListener("click", () => {
+        interestRadio.checked = true
+    })
+}
+checkRadio()
+
+//  Function to show error messages
+function showError(inputId, message){
+    document.getElementById(inputId).textContent = message;
+    resultsCalculation.style.display = "none";
+    emptyResults.style.display = "block";   
+}
+
+
+// Clear all inputs
+const clearAll = document.querySelector(".clear-all")
+clearAll.addEventListener("click", () => {
+    mortgageAmount.value = ""
+    mortgageTerm.value = ""
+    interestRate.value = ""
+})
